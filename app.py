a6f2fceb10463fefe4129f11dc85379be6a815e0
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-07 17:23:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,8 @@ elif page == "⚙️ Configuración":
             fecha_fin = st.date_input("Fecha fin irradiación ((yyyy,mm,dd):", value=datetime(2025, 9, 26))
             st.session_state.fecha_fin = fecha_fin
         with col_hora2:
-            hora_fin = st.time_input("Hora fin irradiación:", value=datetime.strptime("09:45:00", "%H:%M:%S").time(),step=timedelta(seconds=1))
+            #hora_fin = st.time_input("Hora fin irradiación:", value=datetime.strptime("09:45:00", "%H:%M:%S").time(),step=timedelta(seconds=1))
+            hora_ini = st.text_input("Hora fin irradiación (HH:MM:SS):", value="09:45:00")
             st.session_state.hora_fin = hora_fin
         
         st.subheader("📊 Parámetros de Incertidumbre")
