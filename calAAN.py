5472196c2463090ece3a7c383fae5c952d59fee5
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-12-10 12:08:59 -0500

    Update calAAN.py

--- calAAN.py
+++ calAAN.py
@@ -12,7 +12,7 @@ import sympy as sp
 import matplotlib.pyplot as plt
 import tempfile
 import base64
-from math import log
+#from math import log
 # ==============================================================================
 #                        FUNCIONES AUXILIARES CALCULO
 # ==============================================================================
